7f4b430d3f469bb439697cfa51ebe161673031f3
flindblo <flindblo@kth.se>
2015-11-07 20:28:14 +0100

    cloud changed to use message.java

--- Cloud test/GUI/EventHandling.java
+++ Cloud test/GUI/EventHandling.java
@@ -1,30 +1,47 @@
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import netscape.javascript.JSObject;
+import org.mavlink.messages.ja4rtor.msg_command_long;
+import org.mavlink.messages.ja4rtor.msg_mission_item;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class EventHandling {
 
+public class EventHandling {
+    int sysId = 0;
+    int componentId = 0;
     private Socket clientSocket;
     private int port = 12345;
     private WebEngine engine;
+    ObjectOutputStream objectOut;
+    Message msg;
+    byte[] mavLinkByteArray = null;
 
     @FXML
     private Button missionButton;
+    @FXML
+    private Button liftButton;
+    @FXML
+    private Button landButton;
     @FXML
     private WebView webView;
     @FXML
     private TextField latTextfield;
     @FXML
     private TextField lngTextfield;
+    @FXML
+    private CheckBox connectedCheckBox;
+    @FXML
+    private Slider wifiSlider;
 
     public EventHandling() {
         System.out.println("Gui created!");
@@ -36,15 +53,19 @@ public class EventHandling {
         setUpServer();
 
         missionButton.setOnAction((event) -> {
-            PrintWriter out;
-            try {
-                out = new PrintWriter(clientSocket.getOutputStream(), true);
-                out.println("Teeest!");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            flyTo();
+        });
 
-            System.out.println("Button pressed!");
+        liftButton.setOnAction((event) -> {
+            lift();
+        });
+
+        landButton.setOnAction((event) -> {
+            land();
+        });
+
+        wifiSlider.setOnDragDone((event) -> {
+             wifi(wifiSlider.getValue());
         });
 
         engine = webView.getEngine();
@@ -64,7 +85,7 @@ public class EventHandling {
     }
 
     private void setUpServer() {
-        System.out.println("setup!");
+        System.out.println("setup server socket!");
         Task<Socket> task = new Task<Socket>() {
             @Override
             protected Socket call() {
@@ -72,20 +93,147 @@ public class EventHandling {
 
                 try {
                     serverSocket = new ServerSocket(port);
-                    clientSocket = serverSocket.accept();
+                    clientSocket = serverSocket.accept();//The thread is blocked here
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
                 System.out.println("Connection accepted!");
-                Server serverTask = new Server(12346, EventHandling.this, clientSocket);
+                Server serverTask = new Server(12345, EventHandling.this, clientSocket);
                 new Thread(serverTask).start();
                 return clientSocket;
             }
         };
         new Thread(task).start();
 
-        task.setOnSucceeded((workerStateEvent) -> {
+        task.setOnSucceeded((workerStateEvent) -> {//When serverSocket.accept() is done, retrieve the handle to the client and create stream
             clientSocket = task.getValue();
+            connectedCheckBox.setText("Drone connected");
+            connectedCheckBox.setSelected(true);
+            try {
+                objectOut = new ObjectOutputStream(clientSocket.getOutputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            msg = new Message();
         });
     }
+
+    private void flyTo() {
+        msg_mission_item mi = new msg_mission_item(sysId, componentId);
+        mi.target_system = sysId;
+        mi.target_component = componentId;
+        mi.seq = 1;
+        mi.frame = 0;
+        mi.command = 17;
+        mi.current = 0; //No idea what this is
+        mi.autocontinue = 1; //Autocontinue, but there wont be anymore until the next command is sent?
+        mi.param1 = 0;
+        mi.param2 = 0;
+        mi.param3 = 0;
+        mi.param4 = 0;
+        mi.x = Float.parseFloat(latTextfield.getText());
+        mi.y = Float.parseFloat(lngTextfield.getText());
+        mi.z = 0;
+
+        try {
+            mavLinkByteArray = mi.encode();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        msg.setByteArray(mavLinkByteArray);
+        msg.setMessageType(MessageType.MAVLINK);
+
+        try {
+            objectOut.writeObject(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void lift() {
+        msg_mission_item mi = new msg_mission_item(sysId, componentId);
+        mi.target_system = sysId;
+        mi.target_component = componentId;
+        mi.seq = 1;
+        mi.frame = 1;
+        mi.command = 22;
+        mi.current = 0; //No idea what this is
+        mi.autocontinue = 1; //Autocontinue, but there wont be anymore until the next command is sent?
+        mi.param1 = 0;
+        mi.param2 = 0;
+        mi.param3 = 0;
+        mi.param4 = 0;
+        mi.x = Float.parseFloat(latTextfield.getText());
+        mi.y = Float.parseFloat(lngTextfield.getText());
+        mi.z = 0;
+
+        try {
+            mavLinkByteArray = mi.encode();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        msg.setByteArray(mavLinkByteArray);
+        msg.setMessageType(MessageType.MAVLINK);
+
+        msg.setByteArray(mavLinkByteArray);
+        msg.setMessageType(MessageType.MAVLINK);
+        try {
+            objectOut.writeObject(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void land() {
+        msg_mission_item mi = new msg_mission_item(sysId, componentId);
+        mi.target_system = sysId;
+        mi.target_component = componentId;
+        mi.seq = 1;
+        mi.frame = 1;
+        mi.command = 21;
+        mi.current = 0; //No idea what this is
+        mi.autocontinue = 1; //Autocontinue, but there wont be anymore until the next command is sent?
+        mi.param1 = 0;
+        mi.param2 = 0;
+        mi.param3 = 0;
+        mi.param4 = 0;
+        mi.x = Float.parseFloat(latTextfield.getText());
+        mi.y = Float.parseFloat(lngTextfield.getText());
+        mi.z = 0;
+
+        try {
+            mavLinkByteArray = mi.encode();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        msg.setByteArray(mavLinkByteArray);
+        msg.setMessageType(MessageType.MAVLINK);
+
+        msg.setByteArray(mavLinkByteArray);
+        msg.setMessageType(MessageType.MAVLINK);
+        try {
+            objectOut.writeObject(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void wifi(double status) {
+        msg.setByteArray(null);
+
+        if(status == 0) {
+            msg.setMessageType(MessageType.WIFIOFF);
+        }
+        else {
+            msg.setMessageType(MessageType.WIFION);
+        }
+
+        try {
+            objectOut.writeObject(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    msg_command_long cl = new msg_command_long();//testing in progress
 }
--- Cloud test/GUI/Server.java
+++ Cloud test/GUI/Server.java
@@ -4,10 +4,14 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 
 import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 
-public class Server extends Task<Void>{
+public class Server extends Task<Void> {
     private int port;
     private Socket clientSocket;
+    @FXML
+    private CheckBox connectedCheckBox;
 
     public Server(int port, EventHandling handleGUI, Socket clientSocket) {
         this.port=port;
@@ -27,12 +31,18 @@ public class Server extends Task<Void>{
                 if ((msg = in.readLine()) != null) {
                     System.out.println(msg);
                 }
+                else {
+                    return null;
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             in.close();
             clientSocket.close();
+            connectedCheckBox.setText("Drone not connected");//Does not really work
+            connectedCheckBox.setSelected(false);
+            System.out.println("Drone disconnected!");
         }
         return null;
     }
